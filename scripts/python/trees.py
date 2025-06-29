f5b9f32877d3ed7566fe520e942b80816193336e
J-Ngaiii <jngai_@berkeley.edu>
2025-06-29 11:29:11 -0700

    implemented pre and post order from scratch

--- scripts/python/trees.py
+++ scripts/python/trees.py
@@ -115,8 +115,10 @@ class MutativeTraversals (TreeTraversals):
         if iterative:
             queue = deque(children)
             while queue:
+                # process the current node
                 node = queue.popleft()
                 self.apply_func(node)
+                # add the current node's children to be processed
                 queue.extend(node.get_children())
         else:
             for c in children:
@@ -124,6 +126,26 @@ class MutativeTraversals (TreeTraversals):
 
 
     def inorder(self, root, iterative=False):
+        """
+        Inorder traversal order is:
+            - traverse left
+            - process curr node
+            - traversr right
+
+        Cannot simply check if a node has no left child then process and move to next in the stack because --> you can't check for nodes you've already processed
+        Fix:
+        - use a while loop for a left/curr variable to keep traversing in one direction (eg keep going left) until theres no child in that direction
+        - reset the left/curr variable when you process the node
+
+        eg. 
+        - for tree: 
+                    10
+                8       12
+            3       9
+        - initially stack is empty so you rely on curr = root to trigger the while loop
+        - while loop kills after appending 3 to the stack and curr is set equal to None
+        - 3 gets processed then --> reset curr to equal None (which is okay because the stack is still not empty)
+        """
         assert isinstance(root, BinaryTree), "inorder traversal only works on binary tree objects."
         if self.is_leaf(root):
             self.apply_func(root)
@@ -149,5 +171,72 @@ class MutativeTraversals (TreeTraversals):
             self.apply_func(root)
             if right:
                 self.inorder(right, iterative=False)
+
+    def preoreder(self, root, iterative=False):
+        assert isinstance(root, BinaryTree), "inorder traversal only works on binary tree objects."
+        if self.is_leaf(root):
+            self.apply_func(root)
+            return
+        
+        children = root.get_children()
+        if iterative:
+            stack = []
+            stack.append(root)
+            while stack:
+                curr = stack.pop()
+                self.apply_func(curr)
+                right = curr.get_right()
+                if right: stack.append(right)
+                left = curr.get_left()
+                if left: stack.append(left)
+                
+        else:
+            self.apply_func(root)
+            left = root.get_left()
+            right = root.get_right()
+            if left:
+                self.preoreder(left, iterative=False)
+            if right:
+                self.preoreder(right, iterative=False)
+
+    def postorder(self, root, iterative=False):
+        """
+        Postorder iterative needs two stacks because the order of visiting is too different from the order of processing
+
+        eg. 
+        - for tree: 
+                    10
+                8       12
+            3       9
+        - after processing 3 you need to be able to go up to 8 --> then go to 9 without revisiting 3 or processing 8 itself
+        - so you need one datastructure to keep track of nodes you've visited to avoid revisiting and another to keep track of the actual order in which you want to process
+        """
+        assert isinstance(root, BinaryTree), "inorder traversal only works on binary tree objects."
+        if self.is_leaf(root):
+            self.apply_func(root)
+            return
+        
+        children = root.get_children()
+        if iterative:
+            stack = [root] # stack keeps track of the nodes we've visited/children we need to visit as we traverse down the tree
+            output = [] # output keeps nodes we've visited in the correct order so that they get processed in the right order
+            while stack: # while loop is updates the stack --> to keep track of visited nodes AND orders the nodes correctly by adding them to output at the right time
+                node = stack.pop()
+                output.append(node)
+                if node.get_left():
+                    stack.append(node.get_left())
+                if node.get_right():
+                    stack.append(node.get_right())
+            while output: # stack is exhausted => all nodes visited, output is popped in order to carry out postorder processing
+                self.apply_func(output.pop())
+                
+        else:
+            left = root.get_left()
+            right = root.get_right()
+            if left:
+                self.postorder(left, iterative=False)
+            if right:
+                self.postorder(right, iterative=False)
+            self.apply_func(root)
                 
 
